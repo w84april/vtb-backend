bfe1fac5485c903cad56777f97cf8e27e6d1c6ff
Denis <icesfrost@gmail.com>
2022-10-08 04:05:20 +0300

    Add event entity

---
+++ models/Event.js
@@ -0,0 +1,51 @@
+module.exports = (sequelize, DataTypes) => {
+    const Event = sequelize.define(
+        'Event',
+        {
+            id: {
+                type: DataTypes.UUID,
+                allowNull: false,
+                validate: {
+                    notEmpty: true,
+                },
+                primaryKey: true,
+                defaultValue: DataTypes.UUIDV1,
+            },
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    notEmpty: true,
+                },
+            },
+            description: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    notEmpty: true,
+                },
+            },
+            type: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    notEmpty: true
+                }
+            },
+            middleAwardPower: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                validate: {
+                    notEmpty: true,
+                },
+            },
+        },
+        {
+            sequelize,
+            modelName: 'User',
+            freezeTableName: true,
+        },
+    );
+
+    return User;
+};
