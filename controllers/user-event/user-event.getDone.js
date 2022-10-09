6a96882dacbcdb69a0bd4ffb6ee74ef2853634c4
w84april <donyaschefer@gmail.com>
2022-10-09 06:25:45 +0400

    Add getting user events

---
+++ controllers/user-event/user-event.getDone.js
@@ -0,0 +1,26 @@
+const e = require('express');
+const Router = e.Router();
+const { validationResult } = require('express-validator');
+const { User_Event: UserEvent } = require('../../models');
+
+const authorization = require('../../authorization');
+const getDoneUserEvents = Router.get('/user-events', authorization, async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const events = await UserEvent.findAll({
+      where: {
+        done: true,
+      },
+    });
+    const formattedEvents = events.map(event => event.dataValues);
+
+    res.send(formattedEvents);
+  } catch (err) {
+    return res.status(400).send(err.message);
+  }
+});
+module.exports = getDoneUserEvents;
