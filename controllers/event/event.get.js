9dc79545c74b84f5e56845e31e9eabf76dfc8395
Denis <icesfrost@gmail.com>
2022-10-09 06:14:22 +0300

    Extended event get route

--- controllers/event/event.get.js
+++ controllers/event/event.get.js
@@ -1,6 +1,6 @@
 const e = require('express');
 const Router = e.Router();
-const { Event } = require('../../models');
+const { Event, User_Event: UserEvent } = require('../../models');
 const { validationResult } = require('express-validator');
 const authorization = require('../../authorization');
 const getEvents = Router.get('/events', authorization, async (req, res) => {
@@ -11,9 +11,28 @@ const getEvents = Router.get('/events', authorization, async (req, res) => {
     }
 
     const events = await Event.findAll();
-    const formattedEvents = events.map(event => event.dataValues);
+    const takenEventsByUser = await UserEvent.findAll({
+      where: {
+        UserId: res.locals.id
+      }
+    })
 
-    res.send(formattedEvents);
+    const formattedEvents = events.map(event => event.dataValues);
+    const extendedEvetns = formattedEvents.map(event => {
+      return takenEventsByUser.find(u => u.EventId === event.id) ?
+          {
+            ...event,
+            take: true,
+            done: takenEventsByUser.find(u => u.EventId === event.id).done,
+            approve: takenEventsByUser.find(u => u.EventId === event.id).approve,
+          } :
+          {
+           ...event,
+            take: false
+          }
+    })
+
+    res.send(extendedEvetns);
   } catch (err) {
     return res.status(400).send(err.message);
   }
